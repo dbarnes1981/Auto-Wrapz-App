ba3f03ffc65bfecf1f1e061b0d1b43d9f9dd76eb
dbarnes81 <wondery81@gmail.com>
2021-04-03 19:22:33 -0400

    another update

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Image Slider Section
 
 let sliderImages = document.querySelectorAll('.slide-content'),
     arrowLeft = document.querySelector('.fa-chevron-left'),
@@ -54,4 +55,40 @@ let sliderImages = document.querySelectorAll('.slide-content'),
     });
     
     startSlide();
+
+
+    // Scroll Portion
+
+    const scrollClicker = document.querySelector('.scroll');
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+      function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          scrollClicker.style.display = "block";
+        } else {
+          scrollClicker.style.display = "none";
+        }
+      }
+        // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+      }
+    
+// Toggle / Show Side Navigation Section
+
+const toggle = document.querySelector('.fa-bars');
+const closeBtn = document.querySelector('.fa-window-close');
+
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('show-nav');
+});
+    
+
+closeBtn.addEventListener('click', () => {
+  document.body.classList.remove('show-nav');
+});
+    
     
